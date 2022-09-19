7f5cb339e0471ab69aa2ed0330e6f508a4a8c6de
Danila Odinokov <danila_odinokov@outlook.com>
2022-09-19 19:34:50 +0300

    Завершение написания программы

--- Program.cs
+++ Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+string[] str = {"hello","2","world",":-)"};
+string[] result = ArrayCreate(str);
+Console.Write($"{String.Join(" ",result)}");
+
+string[] ArrayCreate(string[] stroka)
+{
+    int count = 0;
+    int i = 0;
+    int resultSize = 0;
+    int len = 3;
+
+    while (count < stroka.Length)
+    {
+        if (stroka[count].Length <= len)
+        {
+            resultSize++;
+        }
+        count++;
+    }
+
+    count = 0;
+    string [] res = new string[resultSize];
+
+    while (count < stroka.Length)
+    {
+        if (stroka[count].Length <= len)
+        {
+            res[i] = stroka[count];
+            i++;
+        }
+        count++;
+    }
+    return res;
+}
